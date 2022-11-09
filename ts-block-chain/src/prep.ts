fa5dea2a2d9eee07f76ef8a9024e60253b583bd4
Sam <wtawonlife@gmail.com>
2022-11-09 21:34:01 +0900

    typescript practice 2

--- ts-block-chain/src/prep.ts
+++ ts-block-chain/src/prep.ts
@@ -95,3 +95,122 @@ let g: any = true; //any could be any types. try to refrain from using this.
 g = 12;
 
 /** 1-4 void/never/unknown  */
+
+let w: unknown;
+let q = w + 1; // error.
+
+if (typeof w === "number") {
+  let s = w + 1;
+}
+
+if (typeof w === "string") {
+  let t = w.toUpperCase();
+}
+
+//void is for a function that doesn't return anything...
+
+function sagen() {
+  console.log("x");
+}
+
+const p = sagen();
+p.toUpperCase();
+
+//never is for a function taht never returns.
+
+function hi(): never {
+  //return "x" ... error
+  throw new Error("error");
+}
+
+function hello(name: string | number) {
+  if (typeof name === "string") {
+    name;
+  } else if (typeof name === "number") {
+    name;
+  } else {
+    name; // this code will never run...
+  }
+}
+
+/** 2-0 call signatures */
+const add = (a: number, b: number) => a + b; //what if you don't want to specify types of parameters?
+
+type Add = (a: number, b: number) => number;
+
+const add2: Add = (a, b) => a + b;
+
+/**2-1 overloading */
+
+//overloading has multiple call signatures...
+type Plus = {
+  (a: number, b: number): number;
+  (a: number, b: string): number;
+};
+
+const plus: Plus = (a, b) => {
+  if (typeof b === "string") return a;
+  return a + b;
+};
+
+type Config = {
+  path: string;
+  state: object;
+};
+
+type Push = {
+  (path: string): void; //returns nothing
+  (config: Config): void;
+  //parameters
+};
+
+const push: Push = (config) => {
+  if (typeof config === "string") {
+    console.log(config);
+  } else {
+    console.log(config.path);
+  }
+};
+
+//when call signatures have different numbers of parameters
+type Adding = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+
+const add3: Adding = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+add3(1, 2, 3);
+
+/** 2-2 polymorphism(many different forms) */
+
+type SuperPrint = {
+  (arr: number[]): void; //concrete type
+  (arr: boolean[]): void;
+  (arr: string[]): void;
+};
+const superPrint: SuperPrint = (arr) => {
+  arr.forEach((i) => console.log(i));
+};
+
+superPrint([1, 2, 3, 4]);
+superPrint([true, false, false]);
+superPrint([1, 2, true, "3"]); //...error
+
+//generic is like a placeholder for typescript
+
+type MegaPrint = {
+  <TypePlaceholder>(arr: TypePlaceholder[]): TypePlaceholder;
+};
+
+const megaPrint: MegaPrint = (arr) => {
+  arr.forEach((i) => console.log(i));
+};
+
+const onePrint: MegaPrint = (arr) => arr[0];
+
+const print_result = onePrint([1, 2, "3", false, null]);
+//typescript is replacing <TypePlaceholder> with the types it finds.
