3e120090257c10cb4d4f0ae441c8c2704d2bf330
Sam <wtawonlife@gmail.com>
2022-11-18 19:01:08 +0900

    making blockchain with typescript

--- ts-block-chain/build/myPackage.js
+++ ts-block-chain/build/myPackage.js
@@ -1,1 +1,25 @@
 "use strict";
+// @ts-check
+// we can use JsDoc
+/**
+ * initialize the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exit = exports.init = void 0;
+function init(config) {
+    return true;
+}
+exports.init = init;
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns {number}
+ */
+function exit(code) {
+    return code + 1;
+}
+exports.exit = exit;
